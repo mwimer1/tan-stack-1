48e1f1aa110983c9ed2ea612c594deccb0151cae
mwimer1 <macrae@getcorso.com>
2024-07-16 14:53:42 -0500

    Initial commit

---
+++ src/wwElement.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="my-element">
+    <table>
+      <thead>
+        <tr>
+          <th v-for="(header, index) in headers" :key="index">{{ header }}</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(row, rowIndex) in rows" :key="rowIndex">
+          <td v-for="(cell, cellIndex) in row" :key="cellIndex">{{ cell }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    content: { type: Object, required: true },
+  },
+  computed: {
+    headers() {
+      return this.content.headers || [];
+    },
+    rows() {
+      return this.content.rows || [];
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.my-element {
+  table {
+    width: 100%;
+    border-collapse: collapse;
+    th, td {
+      border: 1px solid #ddd;
+      padding: 8px;
+    }
+    th {
+      background-color: #f2f2f2;
+      text-align: left;
+    }
+    tr:nth-child(even) {
+      background-color: #f9f9f9;
+    }
+    tr:hover {
+      background-color: #ddd;
+    }
+  }
+}
+</style>
